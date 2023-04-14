6c8df897bcbe7c1387047010bea797929a9384b4
emre <emre@192.168.1.54>
2023-04-14 22:58:05 +0300

    logo is wrapped with Link for redirecting home page

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import "./index.scss";
 
 export default () => {
   return (
     <div className="layoutContainer">
       <header>
-        <h3>Employee App</h3>
+        <Link className="link" to="/">
+          <h3>Employee App</h3>
+        </Link>
       </header>
       <div className="outlet">
         <Outlet />
