cb8e3e42d2b08eaf042ab106820dbbdea895338c
emre <emre@192.168.1.54>
2023-04-14 18:21:59 +0300

    set up server

---
+++ server/src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import http from "http";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+
+dotenv.config();
+
+const app = express();
+const httpServer = http.createServer(app);
+const server = new ApolloServer({});
+await server.start();
+app.use("/graphql", cors(), bodyParser.json(), expressMiddleware(server));
+
+await new Promise((resolve) =>
+  httpServer.listen({ port: process.env.PORT }, resolve)
+);
+
+console.log(`🚀 Server ready at http://localhost:${process.env.PORT}/graphql`);
