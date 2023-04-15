2f354af909c3a01c523bd73d803874e88873f366
emre <emre@192.168.1.56>
2023-04-15 11:12:08 +0300

    env-cmd implemented

--- client/src/providers/ApolloProvider.js
+++ client/src/providers/ApolloProvider.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: process.env.REACT_APP_API_GRAPH_URL,
   cache: new InMemoryCache(),
 });
 
